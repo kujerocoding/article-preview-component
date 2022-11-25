22d23a92c256dff2b44fc563dae983b0e54a3c1f
herom <jerome.manset22@gmail.com>
2022-11-25 22:34:02 +0800

    Add addClassName and removeClassName functions to main.js

--- main.js
+++ main.js
@@ -3,15 +3,31 @@ const shareIconsBox = document.querySelector('.share__container');
 const shareProfileBox = document.querySelector('.shareprofile__container');
 const profileBox = document.querySelector('.profile__container');
 
-
-
-function showIcons () {
-    
+function addClassName () {
     shareBtn.classList.add('share__icon--btn--active');
     shareIconsBox.classList.add('share__container--active');
     shareProfileBox.classList.add('shareprofile__container--bg');
     profileBox.classList.add('profile__container--close');
+}
+
+function removeClassName () {
+    shareBtn.classList.remove('share__icon--btn--active');
+    shareIconsBox.classList.remove('share__container--active');
+    shareProfileBox.classList.remove('shareprofile__container--bg');
+    profileBox.classList.remove('profile__container--close'); 
+}
+
+
+function btnClick () {
+
+    if (shareBtn.classList.contains('share__icon--btn--active')) {
+        removeClassName();          
+    }
+    else {
+        addClassName();
+    }
+
     
 }
 
-shareBtn.addEventListener('click', showIcons);
+shareBtn.addEventListener('click', btnClick);
